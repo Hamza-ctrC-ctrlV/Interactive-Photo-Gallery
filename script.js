62b83348c5de323e798f40128988020aff1a4337
Hamza <hamzahalouani4@gmail.com>
2025-12-30 14:58:33 +0100

    coursera_finals

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function upDate(previewPic) {
-    console.log("Mouse over triggered");
-    console.log(previewPic.alt);
-    console.log(previewPic.src);
+    console.log("Event triggered");
+    console.log("Alt text: " + previewPic.alt);
+    console.log("Source: " + previewPic.src);
+    
     let imageDiv = document.getElementById("image");
-    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
     imageDiv.innerHTML = previewPic.alt;
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
 }
 
 function undo() {
     let imageDiv = document.getElementById("image");
     imageDiv.style.backgroundImage = "url('')";
     imageDiv.innerHTML = "Hover over an image below to display here.";
+}
+
+function addTabFocus() {
+    console.log("Window loaded: addTabFocus function triggered.");
+    let images = document.querySelectorAll(".preview");
+    for (let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabindex", "0");
+        console.log("Tabindex added to image " + (i + 1));
+    }
 }
